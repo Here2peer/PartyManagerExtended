5fedca1820a01193a44de3d37c993ecf0d013eab
Timothy Ravor <timothyravor@live.nl>
2020-04-11 15:22:35 +0200

    Initial commit

---
+++ PartyManagerExtended/PartyManagerExtended.cs
@@ -0,0 +1,36 @@
+﻿using TaleWorlds.CampaignSystem.ViewModelCollection;
+using TaleWorlds.Core;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.MountAndBlade.View.Missions;
+
+using System;
+
+namespace PartyManagerExtended
+{
+    [OverrideView(typeof(PartyCharacterVM))]
+    class PartyManagerExtended : PartyCharacterVM
+    {
+        public PartyManagerExtended(PartyScreenLogic partyScreenLogic, 
+            Action<PartyCharacterVM> setSelected,
+          Action<PartyCharacterVM, int, int, PartyScreenLogic.PartyRosterSide> onTransfer,
+          Action<PartyCharacterVM> onShift,
+          PartyVM partyVm,
+          TroopRoster troops,
+          int index,
+          PartyScreenLogic.TroopType type,
+          PartyScreenLogic.PartyRosterSide side,
+          bool isTroopTransferrable, 
+      string fiveStackShortcutKeyText) : 
+            base(partyScreenLogic, setSelected, onTransfer, onShift, partyVm, troops, index, type, side, isTroopTransferrable, fiveStackShortcutKeyText)
+        {
+
+        }
+        public override void RefreshValues()
+        {
+            base.RefreshValues();
+            this.Name = this.Troop.Character.Name.ToString();
+            this.TradeData?.RefreshValues();
+            new InformationMessage("Mod initialized");
+        }
+    }
+}
---
+++ PartyManagerExtended/PartyManagerExtendedSubModule.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem;
+using CustomCampaignOptions.Behaviours;
+using CustomCampaignOptions.GameModels;
+using StoryMode.GameModels;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents.Party;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace PartyManagerExtended
+{
+    public class PartyManagerExtendedSubModule : MBSubModuleBase
+    {
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            try
+            {
+                if (!(game.GameType is Campaign)) return;
+                if (!(gameStarterObject is CampaignGameStarter gameInitializer)) return;
+                new InformationMessage("Mod initialized");
+                party
+            }
+            catch (Exception e)
+            {
+                new InformationMessage("Mod failed to load");
+            }
+        }
+   
+
+	}
+}
